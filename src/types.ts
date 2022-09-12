fa6ff5f875980e4694bfe921a69afa0c227350c1
Michael Wang <hi@michael.wang>
2022-09-12 10:12:43 -0700

    update

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,7 @@
 import { Endpoints } from "@octokit/types";
 
 export type Codespaces = Endpoints["GET /user/codespaces"]["response"]["data"];
-export type Machines = Endpoints["GET /repos/{owner}/{repo}/codespaces/machines"]["response"]["data"];
+export type Machines =
+  Endpoints["GET /repos/{owner}/{repo}/codespaces/machines"]["response"]["data"];
 export type Codespace = Codespaces["codespaces"][0];
+export type Machine = Machines["machines"][0];
--- src/views/ChangeCompute.tsx
+++ src/views/ChangeCompute.tsx
@@ -1,14 +1,71 @@
-import { Action, ActionPanel, Clipboard, Icon, List, showHUD, showToast, Toast, useNavigation } from "@raycast/api";
+import {
+  Action,
+  ActionPanel,
+  Clipboard,
+  Icon,
+  List,
+  showHUD,
+  showToast,
+  Toast,
+  useNavigation,
+} from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import { personalAccessToken } from "../preferences";
-import { Codespace, Machines } from "../types";
-import fetch from "node-fetch";
+import { Codespace, Machine, Machines } from "../types";
+import handleChangeCompute from "../methods/handleChangeCompute";
 
-const ChangeCompute = ({ codespace, onRevalidate }: { codespace: Codespace; onRevalidate: () => void }) => {
+const ChangeCompute = ({
+  codespace,
+  onRevalidate,
+}: {
+  codespace: Codespace;
+  onRevalidate: () => void;
+}) => {
   const { pop } = useNavigation();
-  const { data, isLoading } = useFetch<Machines>(`${codespace.repository.url}/codespaces/machines`, {
-    headers: { Accept: "application/vnd.github+json", Authorization: `Bearer ${personalAccessToken}` },
-  });
+  const { data, isLoading } = useFetch<Machines>(
+    `${codespace.repository.url}/codespaces/machines`,
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${personalAccessToken}`,
+      },
+    }
+  );
+
+  const onAction = async (machine: Machine) => {
+    const toast = await showToast({
+      title: `Changing compute to ${machine.display_name}...`,
+      style: Toast.Style.Animated,
+    });
+    try {
+      const response = await handleChangeCompute({ codespace, machine });
+      if (response.status !== 200) {
+        const data = (await response.json()) as {
+          message: string;
+          documentation_url: string;
+        };
+        toast.style = Toast.Style.Failure;
+        toast.title = data.message;
+        toast.primaryAction = {
+          title: "Copy link to docs",
+          onAction: () => {
+            Clipboard.copy(data.documentation_url);
+          },
+        };
+      } else {
+        await toast.hide();
+        pop();
+        onRevalidate();
+        await showHUD("Request sent. Compute change may take a few minutes.");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.style = Toast.Style.Failure;
+      toast.title =
+        typeof error === "string" ? error : "Failed to change compute";
+    }
+  };
+
   return (
     <List isLoading={isLoading}>
       <List.Section title="Select compute">
@@ -30,47 +87,7 @@ const ChangeCompute = ({ codespace, onRevalidate }: { codespace: Codespace; onRe
             }
             actions={
               <ActionPanel>
-                <Action
-                  title="Select"
-                  onAction={async () => {
-                    const toast = await showToast({
-                      title: `Changing compute to ${machine.display_name}...`,
-                      style: Toast.Style.Animated,
-                    });
-                    try {
-                      const response = await fetch(`${codespace.url}`, {
-                        method: "PATCH",
-                        headers: {
-                          Accept: "application/vnd.github+json",
-                          Authorization: `Bearer ${personalAccessToken}`,
-                        },
-                        body: JSON.stringify({
-                          machine: machine.name,
-                        }),
-                      });
-                      if (response.status !== 200) {
-                        const data = (await response.json()) as { message: string; documentation_url: string };
-                        toast.style = Toast.Style.Failure;
-                        toast.title = data.message;
-                        toast.primaryAction = {
-                          title: "Copy link to docs",
-                          onAction: () => {
-                            Clipboard.copy(data.documentation_url);
-                          },
-                        };
-                      } else {
-                        await toast.hide();
-                        pop();
-                        onRevalidate();
-                        await showHUD("Request sent. Compute change may take a few minutes.");
-                      }
-                    } catch (error) {
-                      console.log(error);
-                      toast.style = Toast.Style.Failure;
-                      toast.title = "Failed to change compute";
-                    }
-                  }}
-                />
+                <Action title="Select" onAction={() => onAction(machine)} />
               </ActionPanel>
             }
           />
